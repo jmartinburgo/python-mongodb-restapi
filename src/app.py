0ad310e06cd87bd1aa8c6295407182ab8a33721c
Juan Martin Burgo Mangiantini <85910666+jmartinburgo@users.noreply.github.com>
2023-08-04 12:43:01 +0000

    First commit

---
+++ src/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+from flask_pymongo import PyMongo
+
+
+app= Flask(__name__)
+
+app.config['MONGO_URI']='mongodb://mongo:X4JWnfZecc6WlIsDHSuN@containers-us-west-156.railway.app:5573/pythonmongodb'
+
+mongo=PyMongo(app)
+
+@app.route('/users', methods=['POST'])
+
+def create_users():
+    #Recibiendo datos
+    print(request.json)
+    return {'message':'received'}
+
+if __name__ =="__main__":
+    app.run(debug=True)
